94b4b9829a065045a8914569658868f6a7602c5d
Caleb Voorhees <c.voorhees@setonhill.edu>
2023-01-08 15:47:58 -0500

    Initial Commit

---
+++ Seton Hill Welcome App/Seton_Hill_Welcome_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Seton_Hill_Welcome_AppApp.swift
+//  Seton Hill Welcome App
+//
+//  Created by Caleb Voorhees on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Seton_Hill_Welcome_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
